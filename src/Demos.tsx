43e2cff4e29d78224c476db7593133359ca8cffd
Ben Christel <ben.christel@gmail.com>
2024-01-20 17:43:17 -0800

    Move production code to snug.tsx

--- src/Demos.tsx
+++ src/Demos.tsx
@@ -1,6 +1,7 @@
 import {h, Fragment, ComponentChildren} from "preact"
 import {CSSProperties} from "preact/compat"
 import "./classes.css"
+import {Box, Shelf, Stack} from "./snug"
 
 export function Demos() {
   return (
@@ -123,118 +124,6 @@ function Expander() {
       <h1>Hello</h1>
       <h1>Hello</h1>
     </details>
-  )
-}
-
-const tarp = {
-  position: "absolute",
-  inset: 0,
-}
-
-function Module(props: {
-  children: ComponentChildren
-  style?: CSSProperties
-  scroll?: boolean
-}) {
-  const {children, style, scroll} = props
-  const overflow = scroll ? "auto" : "hidden"
-  return <div style={{overflow, ...tarp, ...style}}>{children}</div>
-}
-
-function Stack(props: {
-  children: ComponentChildren
-  style?: CSSProperties
-}) {
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "stretch",
-        ...tarp,
-        ...props.style,
-      }}
-    >
-      {props.children}
-    </div>
-  )
-}
-
-function Shelf(props: {
-  children: ComponentChildren
-  style?: CSSProperties
-}) {
-  return (
-    <div
-      class="snug-shelf"
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "stretch",
-        flexWrap: "wrap",
-        ...props.style,
-      }}
-    >
-      {props.children}
-    </div>
-  )
-}
-
-function Expanse(props: {
-  children?: ComponentChildren
-  style?: CSSProperties
-  scroll?: boolean
-}) {
-  return (
-    <Box expand>
-      <Module style={props.style} scroll={props.scroll}>
-        {props.children}
-      </Module>
-    </Box>
-  )
-}
-
-type BoxProps =
-  | {
-      expand: true
-      clip: true
-      scroll?: boolean
-      children?: ComponentChildren
-      style?: CSSProperties
-    }
-  | {
-      expand: true
-      clip?: false
-      children?: ComponentChildren
-      style?: CSSProperties
-    }
-  | {
-      expand?: false
-      children?: ComponentChildren
-      style?: CSSProperties
-    }
-
-function Box(props: BoxProps) {
-  if (props.expand && props.clip) {
-    return (
-      <Expanse scroll={props.scroll} style={props.style}>
-        {props.children}
-      </Expanse>
-    )
-  }
-  return (
-    <div
-      class={"snug-box" + (props.expand ? " expand" : "")}
-      style={{
-        position: "relative",
-        overflow: "hidden",
-        flexGrow: props.expand ? 1 : 0,
-        flexShrink: 0,
-        ...props.style,
-      }}
-    >
-      {props.children}
-    </div>
   )
 }
 
--- src/snug.tsx
+++ src/snug.tsx
@@ -1,73 +1,113 @@
 import {h, ComponentChildren} from "preact"
 import {CSSProperties} from "preact/compat"
-
 import "./classes.css"
 
-type SnugProps = {
-  children?: ComponentChildren
-  expand?: boolean
-  scroll?: boolean
+const tarp = {
+  position: "absolute",
+  inset: 0,
+}
+
+export function Module(props: {
+  children: ComponentChildren
   style?: CSSProperties
-} & LayoutChildrenOptions
+  scroll?: boolean
+}) {
+  const {children, style, scroll} = props
+  const overflow = scroll ? "auto" : "hidden"
+  return <div style={{overflow, ...tarp, ...style}}>{children}</div>
+}
 
-type LayoutChildrenOptions =
-  | {}
-  | {
-      "column": true
-      "row"?: never
-      "row-wrap"?: never
-      "typography"?: never
-    }
-  | {
-      "column"?: never
-      "row": true
-      "row-wrap"?: never
-      "typography"?: never
-    }
-  | {
-      "column"?: never
-      "row"?: never
-      "row-wrap": true
-      "typography"?: never
-    }
-  | {
-      "column"?: never
-      "row"?: never
-      "row-wrap"?: never
-      "typography": true
-    }
-
-export function Snug(props: SnugProps) {
-  const {scroll = false, style = {}} = props
-
-  const layoutChildren = (() => {
-    if ("row" in props) {
-      return "row"
-    }
-    if ("row-wrap" in props) {
-      return "row-wrap"
-    }
-    if ("column" in props) {
-      return "column"
-    }
-    if ("typography" in props) {
-      return "typography"
-    }
-  })()
-
-  const classes = [
-    "snug",
-    props.expand && "snug-expand",
-    `snug-if-large-content-${scroll ? "scroll" : "stretch"}`,
-    `snug-layout-children-${layoutChildren}`,
-    ("height" in style || "minHeight" in style) &&
-      "snug-fixed-height",
-  ]
-
+export function Stack(props: {
+  children: ComponentChildren
+  style?: CSSProperties
+}) {
   return (
     <div
-      class={classes.filter(Boolean).join(" ")}
-      style={props.style}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "stretch",
+        ...tarp,
+        ...props.style,
+      }}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+export function Shelf(props: {
+  children: ComponentChildren
+  style?: CSSProperties
+}) {
+  return (
+    <div
+      class="snug-shelf"
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "stretch",
+        flexWrap: "wrap",
+        ...props.style,
+      }}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+function Expanse(props: {
+  children?: ComponentChildren
+  style?: CSSProperties
+  scroll?: boolean
+}) {
+  return (
+    <Box expand>
+      <Module style={props.style} scroll={props.scroll}>
+        {props.children}
+      </Module>
+    </Box>
+  )
+}
+
+type BoxProps =
+  | {
+      expand: true
+      clip: true
+      scroll?: boolean
+      children?: ComponentChildren
+      style?: CSSProperties
+    }
+  | {
+      expand: true
+      clip?: false
+      children?: ComponentChildren
+      style?: CSSProperties
+    }
+  | {
+      expand?: false
+      children?: ComponentChildren
+      style?: CSSProperties
+    }
+
+export function Box(props: BoxProps) {
+  if (props.expand && props.clip) {
+    return (
+      <Expanse scroll={props.scroll} style={props.style}>
+        {props.children}
+      </Expanse>
+    )
+  }
+  return (
+    <div
+      class={"snug-box" + (props.expand ? " expand" : "")}
+      style={{
+        position: "relative",
+        overflow: "hidden",
+        flexGrow: props.expand ? 1 : 0,
+        flexShrink: 0,
+        ...props.style,
+      }}
     >
       {props.children}
     </div>
